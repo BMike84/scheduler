d5ae7295e8f5755e428473f6d3574f513c99cf12
Michael Buffone <m_buffone@hotmail.com>
2022-05-18 10:07:19 -0400

    This started all the components for appointments. Its not completed yet and also added in relevant stories

--- stories/index.js
+++ stories/index.js
@@ -10,6 +10,14 @@ import DayListItem from "components/DayListItem";
 import DayList from "components/DayList";
 import InterviewerListItem from "components/InterviewerListItem";
 import InterviewerList from "components/InterviewerList";
+import Appointment from "components/Appointment/index.js";
+import Header from "components/Appointment/Header";
+import Empty from "components/Appointment/Empty";
+import Show from "components/Appointment/Show";
+import Confirm from "components/Appointment/Confirm";
+import Status from "components/Appointment/Status";
+import Error from "components/Appointment/Error";
+import Form from "components/Appointment/Form";
 
 
 // Buttons
@@ -110,6 +118,8 @@ storiesOf("Button", module)
           setInterviewer={() => action("setInterviewer")(interviewer.id)}
         />
       ));
+
+      //interviewList
       const interviewers = [
         { id: 1, name: "Sylvia Palmer", avatar: "https://i.imgur.com/LpaY82x.png" },
         { id: 2, name: "Tori Malcolm", avatar: "https://i.imgur.com/Nmx0Qxo.png" },
@@ -139,3 +149,55 @@ storiesOf("Button", module)
             onChange={action("setInterviewer")}
           />
         ));
+
+        //Appointment
+        storiesOf("Appointment", module)
+          .addParameters({
+            backgrounds: [{ name: "white", value: "#fff", default: true }]
+          })
+          .add("Appointment", () => <Appointment />)
+          .add("Appointment with time", () => <Appointment time="12pm" />)
+          .add("Header", () => <Header time="12pm" />)
+          .add("Empty", () => <Empty onAdd={action("onAdd")} />)
+          .add("Show", () => 
+            <Show 
+              student="Lydia Miller-Jones"
+              interviewer={interviewer}
+              onEdit={action("onEdit")}
+              onDelete={action("onDelete")}
+            />
+          )
+          .add("Confirm", () => 
+            <Confirm 
+              message="Delete the appointment?"
+              onConfirm={action("onConfirm")}
+              onCancel={action("onCancel")}
+            />
+          )
+          .add("Status", () => 
+            <Status 
+              message="Deleting"
+            />
+          )
+          .add("Error", () => 
+            <Error 
+              message="Could not delete appointment."
+              onClose={action("onClose")}
+            />
+          )
+          .add("Edit Form", () => 
+            <Form
+              student="Mike"
+              interviewer={3}
+              interviewers={interviewers}
+              onSave={action("onSave")}
+              onCancel={action("onCancel")}
+            />
+          )
+          .add("Create Form", () => 
+            <Form
+              interviewers={interviewers}
+              onSave={action("onSave")}
+              onCancel={action("onCancel")}
+            />
+          )
