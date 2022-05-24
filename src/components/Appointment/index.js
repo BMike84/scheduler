677e5c53e76a6057ee2e28746621bc9095812452
Michael Buffone <m_buffone@hotmail.com>
2022-05-24 07:42:50 -0400

    This added in create appointments to each slot

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -3,29 +3,35 @@ import "./styles.scss";
 import Header from "./Header";
 import Show from "./Show";
 import Empty from "./Empty";
+import Status from "./Status";
 import useVisualMode from "hooks/useVisualMode";
 import Form from "./Form";
 
 const EMPTY = "EMPTY";
 const SHOW = "SHOW";
 const CREATE = "CREATE";
+const SAVING = "SAVING";
 
 export default function Appointment(props) {
 
   const { mode, transition, back } = useVisualMode(props.interview ? SHOW : EMPTY);
-
-  function save(name, interviewer) {
+  
+  const save = (name, interviewer) => {
     const interview = {
       student: name,
       interviewer
     };
-    props.bookInterview(props.id, interview);
+    transition(SAVING);
+    props.bookInterview(props.id, interview).then(() => transition(SHOW));
   }
 
   return (
     <article className="appointment">
       <Header time={props.time} />
       {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
+      {mode === SAVING && <Status 
+        message="Saving"
+      />}
       {mode === SHOW && (
         <Show
           student={props.interview.student}
