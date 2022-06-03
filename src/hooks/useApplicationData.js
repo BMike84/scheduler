b400e351f17d4824c01f4be36dd8599c23500492
Michael Buffone <m_buffone@hotmail.com>
2022-06-03 12:25:13 -0400

    This fixed update spots for when editing to not update spots on edit

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -2,18 +2,17 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 
 export default function useApplicationData() {
-  
   const [state, setState] = useState({
     day: "Monday",
     days: [],
     appointments: {},
-    interviewers: {}
+    interviewers: {},
   });
 
-  const setDay = day => setState({ ...state, day });
+  const setDay = (day) => setState({ ...state, day });
 
   // used to call the api's using promises
-  useEffect(()=>{
+  useEffect(() => {
     const dayURL = "/api/days";
     const appointmentURL = "/api/appointments";
     const interviewersURL = "/api/interviewers";
@@ -21,49 +20,55 @@ export default function useApplicationData() {
     Promise.all([
       axios.get(dayURL),
       axios.get(appointmentURL),
-      axios.get(interviewersURL)
+      axios.get(interviewersURL),
     ])
-    // if succuessful it will pull all the data from api's
-    .then((all) => {
-      setState(prevState => ({...prevState, days: all[0].data, appointments: all[1].data, interviewers: all[2].data}));
-    })
-  },[]);
+      // if succuessful it will pull all the data from api's
+      .then((all) => {
+        setState((prevState) => ({
+          ...prevState,
+          days: all[0].data,
+          appointments: all[1].data,
+          interviewers: all[2].data,
+        }));
+      });
+  }, []);
 
-
-     
   const bookInterview = (id, interview) => {
     const appointment = {
       ...state.appointments[id],
-      interview: { ...interview }
+      interview: { ...interview },
     };
 
     const appointments = {
       ...state.appointments,
-      [id]: appointment
+      [id]: appointment,
     };
 
     // when a interview is booked it updates the spots remaining on sidebar without having to refresh page
     const days = state.days.map((day) => {
-      if (day.appointments.includes(id)) {
+      if (
+        day.appointments.includes(id) &&
+        state.appointments[id].interview === null
+      ) {
         day.spots -= 1;
       }
       return day;
     });
 
     return axios
-    .put(`/api/appointments/${id}`, {interview})
-    .then (() => setState({...state, appointments, days}))
-  }
+      .put(`/api/appointments/${id}`, { interview })
+      .then(() => setState({ ...state, appointments, days }));
+  };
 
   const cancelInterview = (id) => {
     const appointment = {
       ...state.appointments[id],
-      interview: null
+      interview: null,
     };
 
     const appointments = {
       ...state.appointments,
-      [id]: appointment
+      [id]: appointment,
     };
 
     // when a interview is booked it updates the spots remaining on sidebar without having to refresh page
@@ -75,9 +80,9 @@ export default function useApplicationData() {
     });
 
     return axios
-    .delete(`/api/appointments/${id}`)
-      .then (() => setState({...state, appointments, days}))
-  }
+      .delete(`/api/appointments/${id}`)
+      .then(() => setState({ ...state, appointments, days }));
+  };
 
   return { state, setDay, bookInterview, cancelInterview };
 }
