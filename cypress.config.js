543a053161d804ba41a58f70891f26b52c3f1b62
Michael Buffone <m_buffone@hotmail.com>
2022-06-06 09:59:27 -0400

    This added cypress and its testing

---
+++ cypress.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  viewportWidth: 1280,
+  viewportHeight: 720,
+
+  e2e: {
+    baseUrl: "http://localhost:8000",
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+};
